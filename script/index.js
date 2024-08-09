6339f6557c9ec5870bd97af75e876da3bdc2732d
PrathamManabasannanavar <pratham29052004@gmail.com>
2024-08-09 17:12:01 +0530

    pushed Youtube to Github

---
+++ script/index.js
@@ -0,0 +1,26 @@
+let searchicon = document.getElementById("searchIcon");
+let searchBox = document.getElementById("searchbar");
+let voiceTypingImg = document.getElementById("voiceTypingBox");
+let cancelImage = document.getElementById("cancelImage");
+
+searchBox.addEventListener('keyup', ()=>{
+    if(searchBox.value == ""){
+        searchicon.style.display = "block";
+        voiceTypingImg.style.display = "block"
+        searchBox.style.width = "200px";
+        cancelImage.style.visibility = "hidden";
+        return;
+    }
+    searchicon.style.display = "none";
+    voiceTypingImg.style.display = "none"
+    searchBox.style.width = "400px";
+    cancelImage.style.visibility = "visible";
+});
+
+cancelImage.addEventListener('click', ()=>{
+    searchBox.value = "";
+});
+
+searchicon.addEventListener('click', ()=>{
+    searchBox.focus();
+});
